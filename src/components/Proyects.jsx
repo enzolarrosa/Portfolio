bad10214e7950a617d954ce0d531c3a9d4b36ab4
Nicolas Larrosa <enzolarrosa@gmail.com>
2023-05-29 01:45:01 -0300

    carousel

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import CarouselTwo from "./CarouselTwo";
 import { FormattedMessage } from "react-intl";
 import imgFood1 from '../img/FOOD1.png'
-// import imgFood2 from '../img/FOOD2.png'
-// import imgFood3 from '../img/FOOD3.png'
+import imgFood2 from '../img/FOOD2.png'
+import imgFood3 from '../img/FOOD3.png'
 import imgDog1 from '../img/DOG1.png'
 import imgDog2 from '../img/DOG2.png'
 import imgDog3 from '../img/DOG3.png'
@@ -18,7 +18,7 @@ import imgMundi3 from '../img/MUNDI3.png'
  const slides= [{title: 'Dogs APP',
     img1:imgDog1, img2:imgDog2,img3:imgDog3,obj:'dog',  deploy: 'https://pi-dogs-tau-lovat.vercel.app/', repo:'https://github.com/enzolarrosa/PI-Dogs',tech: 'HTML5, CSS3, React, Javascript, Nodejs, Redux, MySql, Express JS, PostgreSQL.'},{title: 'Food API',
     img1:imgFood1, 
-    // img2:imgFood2,img3:imgFood3, 
+    img2:imgFood2,img3:imgFood3, 
  obj:'food', deploy: 'https://food-larrosa.vercel.app/', repo:'https://github.com/enzolarrosa/Food',tech: 'HTML5, CSS3, React, Javascript, Nodejs, Redux, MySql, Express JS, PostgreSQL.'},
 {title: 'Todo Games',
 img1:imgGame1, img2:imgGame2,img3:imgGame3,obj:'game',  deploy: 'https://todogames.vercel.app/', repo:'https://github.com/enzolarrosa/Todogames', tech: 'HTML5, CSS3, React, Javascript, Nodejs, Redux, MySql, Express JS, PostgreSQL.'},{title: 'Mundi APP',
