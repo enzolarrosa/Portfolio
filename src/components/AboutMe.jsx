888958fb3abc8cf948f054a32211e161acd3786e
Nicolas Larrosa <enzolarrosa@gmail.com>
2023-05-30 06:01:54 -0300

    responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -12,15 +12,15 @@ export default function About(){
             <h2 className="title"><FormattedMessage id="about.title" defaultMessage="About Me" /></h2>
             </div>
             <div className={a.conteiner}>
-             <div className={a.info}>
-             <h3><FormattedMessage id="about.subtitle" defaultMessage="Who I am?" /></h3>
-                 <p><FormattedMessage id="about.parrafo" defaultMessage="My name is Nicolas Larrosa, I am 21 years old, I am a Full Stack Developer focused on web development. I consider myself someone autonomous, self-taught, with great problem solving and good teamwork. Contact me and I can tell you more about me!" /></p>
-             <a href={cv} download='CV Larrosa' className={a.button}><span><FormattedMessage id="about.cv" defaultMessage="Dowloand CV" /></span></a>
-             </div>
              <div className={a.divImage}>
                 <div className={a.contImage}>
                 <img className={a.image} src={yo} alt="imagen mia" />
                 </div>
+             </div>
+             <div className={a.info}>
+             <h3><FormattedMessage id="about.subtitle" defaultMessage="Who I am?" /></h3>
+                 <p><FormattedMessage id="about.parrafo" defaultMessage="My name is Nicolas Larrosa, I am 21 years old, I am a Full Stack Developer focused on web development. I consider myself someone autonomous, self-taught, with great problem solving and good teamwork. Contact me and I can tell you more about me!" /></p>
+             <a href={cv} download='CV Larrosa' className={a.button}><span><FormattedMessage id="about.cv" defaultMessage="Dowloand CV" /></span></a>
              </div>
             </div>
         </div>
